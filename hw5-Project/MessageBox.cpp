1023bf17bb85df4069b4bf32d93eb4c882f29796
omri isac <omri@omris-macbook-pro.local>
2018-01-22 02:02:26 +0200

    MessageBox (cpp+h) 22/1 02:02

--- hw5-Project/MessageBox.cpp
+++ hw5-Project/MessageBox.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 
-
+MessageBox::MessageBox(string user_name) : user_name_(user_name){
+    conversations_ = *new list<MySharedPtr<Conversation>>;
+};
 
 
 //Interface
-
-
 
 
 void MessageBox::VrtDo(string cmdLine, string activeUsrName)
@@ -17,7 +17,20 @@ void MessageBox::VrtDo(string cmdLine, string activeUsrName)
 	vector<string> cmdLineTokens = StringSplit(cmdLine, BLANK_SPACES);
 	if (cmdLineTokens[0] == "New" && cmdLineTokens.size() > 1) // New
 	{
-		// add code here
+        cmdLineTokens.erase(cmdLineTokens.begin());
+        if(isRep(cmdLineTokens))
+           cout << CONVERSATION_FAIL_USER_REPETITION << endl;
+        else if(allUsersExist(cmdLineTokens))
+           cout << CONVERSATION_FAIL_NO_USER << endl;
+        else{
+            set<User> users = vec2set(cmdLineTokens);
+            MySharedPtr<Conversation> conversation = new MySharedPtr<Conversation>(*users);
+            conversations_.push_front(conversation);
+            
+            
+           }
+           
+           
 	}
 	else if (cmdLineTokens[0] == "Open" && cmdLineTokens.size() == 2) // Open
 	{
@@ -39,7 +52,13 @@ void MessageBox::VrtDo(string cmdLine, string activeUsrName)
         cout << INVALID_INPUT;
 }
 
-
+//**************************************************************************************************
+//* function name   :   Preview
+//* Description     :   prints all conversations partners and prints if the specific user has read the
+//*                     message.
+//* Parameters      :   the wanted user name
+//* Return value    :   nothing (void function).
+//**************************************************************************************************
 void MessageBox::Preview(string activeUsrName)
 {
 	int count = 1;
@@ -48,14 +67,14 @@ void MessageBox::Preview(string activeUsrName)
 	else
 		cout << "Conversations:" << endl;
    
-    list<Conversation>::iterator itr = conversations_.begin();
+    list<MySharedPtr<Conversation>>::iterator itr = conversations_.begin();
     for (; itr != conversations_.end(); itr++) // iterate over conversations with iterator called itr
 	{
 		cout << count << ") ";
-		if () //* the user did not read the current message
+		if (itr->isRead(activeUsrName)) //* the user did not read the current message
 			cout << "(UNREAD) ";
 		cout << "Participants: ";
-		(*itr)->DisplayParticipants();
+		itr->DisplayParticipants();
 		++count;
 	}
 }
--- hw5-Project/MessageBox.h
+++ hw5-Project/MessageBox.h
@@ -5,17 +5,16 @@
 #include <list>
 #include "ObjectInterface.h"
 #include "Conversation.h"
+#include "MySharedPtr.h"
 
 class MessageBox : public ObjectInterface {
 public:
+    MessageBox(string user_name); // constructor
     void VrtDo(string cmd, string activeUsrName);
     void Preview(string activeUsrName);
     void Help() const;
 private:
-    MessageBox(); // constructor
-    
-    
-    list<Conversation> conversations_; // conversations list
+    list<MySharedPtr<Conversation>> conversations_; // conversations list
     string user_name_; // user name of the messageBox owner
 };
 
